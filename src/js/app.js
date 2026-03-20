ec84f833c7a20a63047fa0962a7c3335179c89f6
Julien Camus <julien.camus@sonarsource.com>
2026-03-20 16:17:20 +0100

    feat: add user greeting and multi-language format functions

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,22 @@
 var hello = "Hello, World!";
 
 var acceptMe = "Accept Me!";
+
+function greetUser(name) {
+  if (name == null || name == "") {
+    return hello;
+  }
+  return "Hello, " + name + "!";
+}
+
+function formatGreeting(name, language) {
+  var greeting;
+  if (language == "fr") {
+    greeting = "Bonjour";
+  } else if (language == "es") {
+    greeting = "Hola";
+  } else {
+    greeting = "Hello";
+  }
+  return greeting + ", " + name + "!";
+}
