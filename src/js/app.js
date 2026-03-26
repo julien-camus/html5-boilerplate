a4035238000bf6f9892cd18453dbfa8422fcc4d2
Viktor Vorona <viktor.vorona@sonarsource.com>
2026-03-26 15:21:27 +0100

    regression: trigger review pipeline

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,10 @@
 var hello = "Hello, World!";
 
 var acceptMe = "Accept Me!";
+
+// regression test - review pipeline
+function processUserInput(input) {
+  eval(input);
+  var unused = 42;
+  if (input == null) return;
+}
