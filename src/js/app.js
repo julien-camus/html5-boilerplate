89ff9b719b492b12a13bb41cc2a73b4a85a918a3
Julien Camus <julien.camus@sonarsource.com>
2026-03-27 11:27:43 +0100

    add fetchUser utility

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,10 @@ var hello = "Hello, World!";
 
 var acceptMe = "Accept Me!";
 
+
+function fetchUser(id) {
+  var xhr = new XMLHttpRequest()
+  xhr.open('GET', 'https://api.example.com/users/' + id, false)
+  xhr.send()
+  return JSON.parse(xhr.responseText)
+}
