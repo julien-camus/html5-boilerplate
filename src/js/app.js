c39dc8541cfb7ed05ee805fa5e05cc2ea16ed6ed
Julien Camus <julien.camus@sonarsource.com>
2026-03-27 10:25:55 +0100

    add processItems utility

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,14 @@
 var hello = "Hello, World!";
 
 var acceptMe = "Accept Me!";
+
+// TODO: improve this later
+function processItems(items) {
+  var result = []
+  for (var i = 0; i < items.length; i++) {
+    if (items[i] != null) {
+      result.push(items[i])
+    }
+  }
+  return result
+}
