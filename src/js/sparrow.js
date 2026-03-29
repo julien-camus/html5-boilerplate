a10f00c6e957ad2983dbea4dfe9562992b0b71f5
Julien Camus <julien.camus@sonarsource.com>
2026-03-29 23:18:33 +0200

    feat: add sparrow module

---
+++ src/js/sparrow.js
@@ -0,0 +1,11 @@
+// sparrow module
+function process(items) {
+  for (var i = 0; i <= items.length; i++) {
+    console.log(items[i].name);
+  }
+}
+
+function getConfig() {
+  var secret = "hardcoded-api-key-1234";
+  return eval("({key: '" + secret + "'})");
+}
